60a2c51735ef48e9f13ab7f99f502e3eb123e8cb
jimmymaise <the@duyet.com>
2020-12-28 16:04:01 +0700

    update item_transform
    Signed-off-by: Hung Le <hung.lemanh@hiip.asia>

--- item_transform/identity_item_transform_handler.py
+++ item_transform/identity_item_transform_handler.py
@@ -8,39 +8,40 @@ class IdentityItemTransformHandler(BaseItemTransformHandler):
         super().__init__()
 
     def process_item(self, load_items, crawl_items):
-        _user_data = self.get_users_data(load_items, crawl_items)
-        _post_data = self.get_posts_data(load_items, crawl_items)
+        _user_data = self.get_users_data(crawl_items)
+        _post_data = self.get_posts_data(crawl_items)
 
         # Validate schema
         user_data, error_user_data = self._validate_schema(_user_data, UsersObjectSchema)
         post_data, error_post_data = self._validate_schema(_post_data, PostsObjectSchema)
 
         # Build object to store
-        user_objs = self.build_user_object(user_data)
-        post_objs = self.build_user_object(user_data)
+        user_objs = self.build_user_object(user_data, load_items)
+        post_objs = self.build_user_object(post_data, load_items)
 
         return user_objs, post_objs
 
     @staticmethod
-    def get_users_data(load_items, crawl_items):
+    def get_users_data(crawl_items):
         user_objs = {}
 
         return user_objs
 
     @staticmethod
-    def get_posts_data(load_items, crawl_items):
+    def get_posts_data(crawl_items):
         post_objs = {}
 
         return post_objs
 
     @staticmethod
-    def build_user_object(user_data):
+    def build_user_object(user_data, load_items):
         _user_objs = {}
 
         return _user_objs
 
     @staticmethod
-    def build_post_object(post_data):
+    def build_post_object(post_data, load_items):
         _post_objs = {}
 
         return _post_objs
+
