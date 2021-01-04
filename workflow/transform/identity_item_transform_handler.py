53affc400b9f9158c4654c296483e12b68aad0b9
jimmymaise <the@duyet.com>
2021-01-04 17:30:27 +0700

    update coding isssue

--- workflow/transform/identity_item_transform_handler.py
+++ workflow/transform/identity_item_transform_handler.py
@@ -1,6 +1,6 @@
 from workflow.transform.base_item_transform_handler import BaseItemTransformHandler
-from .schema_objects.posts_object_schema import PostsObjectSchema
-from .schema_objects.users_object_schema import UsersObjectSchema
+from workflow.transform.schema_objects.posts_object_schema import PostsObjectSchema
+from workflow.transform.schema_objects.users_object_schema import UsersObjectSchema
 
 
 class IdentityItemTransformHandler(BaseItemTransformHandler):
@@ -8,40 +8,42 @@ class IdentityItemTransformHandler(BaseItemTransformHandler):
         super().__init__()
 
     def process_item(self, loaded_item, collected_data):
-        _user_data = self.get_users_data(collected_data)
-        _post_data = self.get_posts_data(collected_data)
+        user_data = self._extract_user_data_from_collected_data(collected_data)
+        post_data = self._extract_post_data_from_collected_data(collected_data)
 
         # Validate schema
-        user_data, error_user_data = self._validate_schema(_user_data, UsersObjectSchema)
-        post_data, error_post_data = self._validate_schema(_post_data, PostsObjectSchema)
+        user_data, error_user_data = self._validate_schema(user_data, UsersObjectSchema)
+        post_data, error_post_data = self._validate_schema(post_data, PostsObjectSchema)
+
+        if error_user_data and error_post_data:
+            raise Exception('Invalid extracted data schema')
 
         # Build object to store
-        user_objs = self.build_user_object(user_data, loaded_item)
-        post_objs = self.build_user_object(post_data, loaded_item)
+        user_obj = self._build_user_object(user_data, loaded_item)
+        post_obj = self._build_user_object(post_data, loaded_item)
 
-        return user_objs, post_objs
+        return user_obj, post_obj
 
     @staticmethod
-    def get_users_data(collected_data):
+    def _extract_user_data_from_collected_data(collected_data):
+        user_data = {}
+
+        return user_data
+
+    @staticmethod
+    def _extract_post_data_from_collected_data(collected_data):
+        post_data = {}
+
+        return post_data
+
+    @staticmethod
+    def _build_user_object(user_data, loaded_item):
         user_objs = {}
 
         return user_objs
 
     @staticmethod
-    def get_posts_data(collected_data):
+    def _build_post_object(post_data, loaded_item):
         post_objs = {}
 
         return post_objs
-
-    @staticmethod
-    def build_user_object(user_data, loaded_item):
-        _user_objs = {}
-
-        return _user_objs
-
-    @staticmethod
-    def build_post_object(post_data, loaded_item):
-        _post_objs = {}
-
-        return _post_objs
-
