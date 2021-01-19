013b82936ff221d80972b098f2ec61e0e697fbb0
jimmymaise <the@duyet.com>
2021-01-19 23:36:16 +0700

    make more clean in constant

--- core/utils/constant.py
+++ core/utils/constant.py
@@ -1,19 +1,25 @@
 class Constant:
     AM_MAX_REQUEST = 3
     AM_DEFAULT_SLEEP_TIME = 3
+
     SERVICE_CONFIGS_FILE_NAME = 'service_configs_sample.json'
+
     TOP_LEVEL_SCOPE = '__main__'
     LOG_FORMAT = '%(asctime)s - %(name)s: [%(levelname)s]: %(message)s'
+
     SOCIAL_TYPE_PROFILE = 'facebook'
     SOCIAL_TYPE_FAN_PAGE = 'facebook_page'
     SOCIAL_NETWORK_FACEBOOK = 'facebook'
+
     SERVICE_NAME_SEARCH_REPORT = 'search_report'
+
     COLLECTION_NAME_REPORT = 'reports'
     COLLECTION_NAME_KOL = 'kols'
     COLLECTION_NAME_USER = 'users'
     COLLECTION_NAME_MEDIA = 'medias'
     COLLECTION_NAME_POST = 'posts'
     COLLECTION_SERVICE_ERROR_NAME = 'collection_service_error_name'
+
     SLACK_DEFAULT_NOTIFICATION_FIELD = 'text'
 
 
