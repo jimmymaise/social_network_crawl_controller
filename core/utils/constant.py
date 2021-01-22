5a5e39ea55c1790336c997c91a85ed858483e07f
jimmymaise <the@duyet.com>
2021-01-22 16:27:04 +0700

    add support maximum_days_after_taken

--- core/utils/constant.py
+++ core/utils/constant.py
@@ -1,7 +1,7 @@
 class Constant:
     AM_MAX_REQUEST = 3
     AM_DEFAULT_SLEEP_TIME = 3
-
+    DEFAULT_MAXIMUM_DAYS_AFTER_TAKEN = 7
     SERVICE_CONFIGS_FILE_NAME = 'service_configs_sample.json'
 
     TOP_LEVEL_SCOPE = '__main__'
--- workflow/loading/query/report_query.py
+++ workflow/loading/query/report_query.py
@@ -1,11 +1,18 @@
+import time
+from core.utils.constant import Constant
 from workflow.loading.query.base_query import Query
 
 
 class ReportQuery:
     @staticmethod
     def get_reports_for_search_report_service(service_config: dict):
+        maximum_days_after_taken = Constant.DEFAULT_MAXIMUM_DAYS_AFTER_TAKEN
+        maximum_seconds_after_taken = maximum_days_after_taken * 3600 * 24
+        current_timestamp = int(time.time())
+
         filter_ = {
             'country_code': {'$in': service_config.get('MARKET')},
+            'taken_at_timestamp': {'$gt': current_timestamp - maximum_seconds_after_taken},
             f'{service_config["service_name"]}_status.status': {'$nin': ['success']},
         }
         filter_.pop('country_code') if not service_config.get('MARKET') else None
