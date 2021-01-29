783cab930483b4b315830f39d209313189292a0e
jimmymaise <the@duyet.com>
2021-01-29 10:06:40 +0700

    update config

--- config/service_config.py
+++ config/service_config.py
@@ -13,8 +13,11 @@ class ServiceConfigs:
     def get_service_config() -> dict:
         if os.environ.get('SERVICE_CONFIG'):
             return json.loads(base64.b64decode(os.environ.get('SERVICE_CONFIG')))
-        service_config_file_path = f'{str(Path(__file__).resolve().parents[0])}' \
-                                   f'/{Constant.SERVICE_CONFIG_FOLDER_NAME}/{SystemConfig.DEFAULT_SERVICE_LOCAL}.json'
-        with open(service_config_file_path, 'r') as service_config_file:
-            service_config_str = service_config_file.read()
-            return json.loads(service_config_str)
+
+        elif os.environ.get('ENV', '').lower() == 'local':
+            service_config_file_path = f'{str(Path(__file__).resolve().parents[0])}' \
+                                       f'/{Constant.SERVICE_CONFIG_FOLDER_NAME}/{SystemConfig.DEFAULT_SERVICE_LOCAL}.json'
+            with open(service_config_file_path, 'r') as service_config_file:
+                service_config_str = service_config_file.read()
+                return json.loads(service_config_str)
+        raise Exception('Should have SERVICE_CONFIG if not running local')
