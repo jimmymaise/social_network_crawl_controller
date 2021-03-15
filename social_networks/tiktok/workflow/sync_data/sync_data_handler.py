6f4fd8dfb1bd7acf4895ee55a710a313a95833d5
jimmymaise <the@duyet.com>
2021-03-15 21:17:45 +0700

    update user_post_list_request

--- social_networks/tiktok/workflow/sync_data/sync_data_handler.py
+++ social_networks/tiktok/workflow/sync_data/sync_data_handler.py
@@ -1,6 +1,7 @@
 import json
 
 from core.workflows.sync_data.base_sync_data_handler import BaseSyncDataHandler
+from social_networks.tiktok.utils.constant import Constant
 
 
 class SyncDataHandler(BaseSyncDataHandler):
@@ -9,11 +10,11 @@ class SyncDataHandler(BaseSyncDataHandler):
 
     def user_post_list_request(self, user_id, username, sec_uid, queue_name):
         request_message = {
-            'user_id': user_id,
-            'username': username,
+            'social_user_id': user_id,
+            'social_user_name': username,
             'sec_uid': sec_uid,
             'service_name': 'post_list_collection',
-            'social_type': 'tiktok'
+            'social_type': Constant.SOCIAL_NETWORK_TIKTOK
         }
         self.sqs_handler.send_sqs_message(message_body=json.dumps(request_message),
                                           queue_name=queue_name)
