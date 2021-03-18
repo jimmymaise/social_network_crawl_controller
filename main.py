21dfc2fccc7508db4b181e03b5e2639474f62932
jimmymaise <the@duyet.com>
2021-03-18 14:31:49 +0700

    Run service on mode 'scheduling'

--- main.py
+++ main.py
@@ -60,14 +60,14 @@ def lambda_handler(event, context):
 
 
 if __name__ == '__main__':
-    # ServicesRunner().run()
-    message = {"social_type": "tiktok",
-               "social_name": "cuti",
-               "social_user_name": "lebong95",
-               "country_code": "vi",
-               "hiip_user_id": 1234,
-               "sec_uid": 'MS4wLjABAAAAL7fKextCmJnaMSNxmPYnsxqfXoYgJs9r8fd7viWLA-0hSxrLM8wFPtaKEtWcI51R',
-               "taken_at_timestamp": 11,
-               "service_name": "post_list_collection"}
-    data = {"Records": [{'body': json.dumps(message)}]}
-    lambda_handler(data, {})
+    ServicesRunner().run()
+    # message = {"social_type": "tiktok",
+    #            "social_name": "cuti",
+    #            "social_user_name": "lebong95",
+    #            "country_code": "vi",
+    #            "hiip_user_id": 1234,
+    #            "sec_uid": 'MS4wLjABAAAAL7fKextCmJnaMSNxmPYnsxqfXoYgJs9r8fd7viWLA-0hSxrLM8wFPtaKEtWcI51R',
+    #            "taken_at_timestamp": 11,
+    #            "service_name": "post_list_collection"}
+    # data = {"Records": [{'body': json.dumps(message)}]}
+    # lambda_handler(data, {})
