dab76bab1f332ee416517c6a75c16067a2acaa05
jimmymaise <the@duyet.com>
2021-03-19 10:55:06 +0700

    fix bug s3 kol

--- core/handlers/file_handler/s3_handler.py
+++ core/handlers/file_handler/s3_handler.py
@@ -64,17 +64,19 @@ class S3Handler:
         return self.s3_session
 
     def upload(self, filename, bucket, s3_path=None, s3_full_path=None, callback=None, extra_args=None):
+        if not extra_args:
+            extra_args = {}
         if not s3_full_path:
             if not s3_path:
                 raise ValueError('We must have s3_full_path or s3_path')
             s3_full_path = os.path.join(s3_path, os.path.basename(filename))
         s3_full_path = str(pathlib.Path(s3_full_path))
-        if not extra_args or not extra_args.get('ContentType'):
-            guessed_mime_type = mimetypes.guess_type(filename)
+        if not extra_args.get('ContentType') and mimetypes.guess_type(filename):
+            guessed_mime_type = mimetypes.guess_type(filename)[0]
             extra_args['ContentType'] = guessed_mime_type
 
-        self.s3_client.upload_file(filename, bucket, s3_full_path,
-                                   callback, extra_args=extra_args)
+        self.s3_client.upload_file(filename, bucket, s3_full_path, CallBack=callback,
+                                   ExtraArgs=extra_args)
         s3_url = 's3://{bucket}/{key}'.format(bucket=bucket, key=s3_full_path)
         return s3_url
 
